df2a5b1a6f265e0dcc1883f5dfc7daa1250fba8d
Fırat Topcu <ftopcu@sabanciuniv.edu>
2022-09-28 16:37:24 +0300

    created 3 pieces for each player

--- TiciTaca-Prototype/Assets/Scripts/Piece.cs
+++ TiciTaca-Prototype/Assets/Scripts/Piece.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Piece : MonoBehaviour
 {
     public int pieceValue;
     private Color pieceColor;
+    public TMPro.TextMeshProUGUI writtenPieceValue;
 
     public Piece(Color color, int value)
     {
         pieceValue = value;
         pieceColor = color;
+    }
+
+    private void SetPieceValue(int value)
+    {
+        writtenPieceValue.text = value.ToString();
     }
 }
