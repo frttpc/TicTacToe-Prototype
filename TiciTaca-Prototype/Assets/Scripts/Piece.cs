cf069a35ac24aa34c82c7bb5bd7014858009c117
Fırat Topcu <ftopcu@sabanciuniv.edu>
2022-09-28 15:34:11 +0300

    Turnin playable Pieces to UI elements

--- TiciTaca-Prototype/Assets/Scripts/Piece.cs
+++ TiciTaca-Prototype/Assets/Scripts/Piece.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    public int value;
+    public int pieceValue;
     private Color pieceColor;
 
-    public Piece()
+    public Piece(Color color, int value)
     {
-
+        pieceValue = value;
+        pieceColor = color;
     }
 }
--- TiciTaca-Prototype/Assets/Scripts/Tile.cs
+++ TiciTaca-Prototype/Assets/Scripts/Tile.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    private int containingValue;
 
 }
